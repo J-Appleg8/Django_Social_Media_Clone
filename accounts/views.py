64de48d5f1da094df5ed2144b7cb60be510b50a1
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2022-01-08 13:13:51 -0800

    set up navbar and login/sign up

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from . import forms
+from .forms import UserCreateForm
 
 
 class SignUp(CreateView):
-    pass
+    form_class = UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
