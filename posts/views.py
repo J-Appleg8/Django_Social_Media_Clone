b0962dc3dfd427fb3b7aa8c7c57547b64d0925fe
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2022-01-08 16:31:27 -0800

    completed the posts templates

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,4 @@
 from django.core.checks import messages
-from django.db.models import query
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.http import Http404
